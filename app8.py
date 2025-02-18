fd813679a2b7f726efdbcae64f2e606b89ad3dcc
Sergiy Bondarenko <bs.kyiv@gmail.com>
2025-02-18 19:30:02 +0200

    Commit 15 dogs

--- app8.py
+++ app8.py
@@ -1,10 +1,15 @@
 # Тренировка по атрибутам классов
 
+class MyClass:
+    def my_method(self):
+        print("Здравствуйте")
+
 class Humans:
     def __init__(self, name, age, age_of_child):
         self.name = name
         self.age = age
         self.age_of_child = age_of_child
+        self.caramba()
 
     def sum_of_all_age(self):
         print(self.age + self.age_of_child)
@@ -12,6 +17,14 @@ class Humans:
     def sum_of_all_age2(self):
         self.sum_of_all_age()
         print(self.age + self.age_of_child +2)
+
+    def caramba (self):
+        print("caramba")
+
+
+# Правильное применение:
+bob_the_object = MyClass()  # Познакомьтесь, это Боб – экземпляр класса.
+bob_the_object.my_method()  # Бобу поручено поприветствовать вас!
 
 man = Humans("John", 25, 40)
 man.sum_of_all_age()
