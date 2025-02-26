4118ffe61940074c73de62952677915bc228765c
Your Name <you@example.com>
2025-02-26 12:54:39 +0200

    Commit 33 dog

--- app10.py
+++ app10.py
@@ -5,24 +5,19 @@ class MyClass:
         print("Здравствуйте")
 
 class Humans:
-    def __init__(self, name="Sara", age=33, age_of_child=25):
+    def __init__(self, name, age, age_of_child):
         self.name = name
         self.age = age
         self.age_of_child = age_of_child
         self.caramba()
-        mike = Ben()
-        self.rondo = 78
-
+        a = Ben()
 
     def sum_of_all_age(self):
         print(self.age + self.age_of_child)
-        self.proba = 232
-        pass
 
     def sum_of_all_age2(self):
         self.sum_of_all_age()
         print(self.age + self.age_of_child +2)
-        pass
 
     def caramba (self):
         print("caramba")
@@ -32,15 +27,6 @@ class Ben:
         pass
         print("supercBen")
 
-    def test(self):
-        a=5
-        b=7
-        c = a+b
-        print(c)
-
-    def testprint(self):
-        c = 13
-        print(c)
 
 # Правильное применение:
 bob_the_object = MyClass()  # Познакомьтесь, это Боб – экземпляр класса.
@@ -50,18 +36,4 @@ man = Humans("John", 25, 40)
 man.sum_of_all_age()
 man.sum_of_all_age2()
 
-woman = Humans()
-woman.sum_of_all_age()
-woman.sum_of_all_age2()
-
-john = Ben()
-john.test()
-john.testprint()
-
-
-
-print(dir(bob_the_object))
 print(dir(man))
-print(dir(john))
-
-print(dir(woman))
---
+++ app17.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designereXmtxQ.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QMainWindow,
+    QMenuBar, QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(681, 468)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.checkBox = QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(60, 50, 85, 20))
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(220, 100, 103, 32))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 681, 37))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+    # retranslateUi
